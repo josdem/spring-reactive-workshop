9ea3f5f3faf22dfeb0c59991785b4a800c40e473
josdem <joseluis.delacruz@gmail.com>
2020-03-10 16:02:02 -0400

    Returning nickname as split value

--- src/main/java/com/jos/dem/spring/reactive/workshop/service/impl/DataBufferStreamerImpl.java
+++ src/main/java/com/jos/dem/spring/reactive/workshop/service/impl/DataBufferStreamerImpl.java
@@ -1,20 +1,15 @@
 package com.jos.dem.spring.reactive.workshop.service.impl;
 
 import com.jos.dem.spring.reactive.workshop.service.DataBufferStreamer;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferFactory;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
 public class DataBufferStreamerImpl implements DataBufferStreamer {
 
-  private final DataBufferFactory dataBufferFactory = new DefaultDataBufferFactory();
-
   @Override
   public Flux<String> stream(String nickname) {
-    return Flux.just("1","2","3");
+    return Flux.just(nickname.split(""));
   }
 
 }
