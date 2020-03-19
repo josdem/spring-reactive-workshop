7905a5deaf0639cd992fff0afb75057e157afd49
josdem <joseluis.delacruz@gmail.com>
2020-03-19 17:40:10 -0400

    #3 Overriding on error

--- src/test/java/com/jos/dem/spring/reactive/workshop/BackPressureStreamServiceTest.java
+++ src/test/java/com/jos/dem/spring/reactive/workshop/BackPressureStreamServiceTest.java
@@ -3,6 +3,8 @@ package com.jos.dem.spring.reactive.workshop;
 import com.jos.dem.spring.reactive.workshop.service.BackPressureStreamService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
@@ -18,13 +20,18 @@ public class BackPressureStreamServiceTest {
 
   @Autowired private BackPressureStreamService backPressureStreamService;
 
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
   @Test
   @DisplayName("should get timed nicknames")
   void shouldGetTimedNicknames() {
     List<String> result = new ArrayList<>();
 
     Flux<String> nicknames = backPressureStreamService.getTimedNicknames();
-    nicknames.subscribe(result::add);
+    nicknames.subscribe(
+        nickname -> result.add(nickname),
+        error -> log.info("Timeout reached: {}", error),
+        () -> log.info("Completed"));
 
     assertEquals(Arrays.asList("josdem", "tgrip", "edzero", "skuarch", "jeduan"), nicknames);
   }
