51326aa7a29019c1829e5594b07146bb7a3744f5
josdem <joseluis.delacruz@gmail.com>
2020-03-22 15:59:35 -0400

    #4 Adding interface definition

---
+++ src/main/java/com/jos/dem/spring/reactive/workshop/service/PersonScheduledStreamService.java
@@ -0,0 +1,8 @@
+package com.jos.dem.spring.reactive.workshop.service;
+
+import com.jos.dem.spring.reactive.workshop.model.Person;
+import reactor.core.publisher.Flux;
+
+public interface PersonScheduledStreamService {
+    Flux<Person> createPublisherThread();
+}
