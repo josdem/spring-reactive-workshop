d03cfb1ffedccab449ac60402a2cbcf3069ad778
josdem <joseluis.delacruz@gmail.com>
2020-03-24 17:46:14 -0400

    #6 Adding test getting ordered nicknames waiting two seconds before

--- src/test/java/com/jos/dem/spring/reactive/workshop/PersonVerifierTest.java
+++ src/test/java/com/jos/dem/spring/reactive/workshop/PersonVerifierTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 @SpringBootTest
 public class PersonVerifierTest {
 
@@ -17,6 +19,22 @@ public class PersonVerifierTest {
   void shouldGetOrderedNicknames() {
     StepVerifier.create(personStreamService.getOrderedNicknames())
         .expectSubscription()
+        .expectNext("edzero")
+        .expectNext("jeduan")
+        .expectNext("josdem")
+        .expectNext("skuarch")
+        .expectNext("tgrip")
+        .thenCancel()
+        .log()
+        .verify();
+  }
+
+  @Test
+  @DisplayName("get ordered nicknames with await time")
+  void shouldWaitTwoSecondsBeforeVerifyOrderedNicknames() {
+    StepVerifier.create(personStreamService.getOrderedNicknames())
+        .expectSubscription()
+        .thenAwait(Duration.ofSeconds(2))
         .expectNext("edzero")
         .expectNext("jeduan")
         .expectNext("josdem")
